9e699b3636c9768c64fd04759a0dfa7ae859e5a5
Shubham Prajapati <exploshubhs@gmail.com>
2018-04-01 21:31:53 +0530

    Created New Table InvoiceDetails 01042018

---
+++ NewTable/InvoiceDetails.sql
@@ -0,0 +1,4 @@
+"ERROR:  syntax error at or near "CREATE"
+LINE 1: ...ARE "CURSOR:CONN:3309944" CURSOR WITHOUT HOLD FOR CREATE TAB...
+                                                             ^
+"
