b7c8432713768e5c9ba2fe81d3c69f44e880c3ad
Shubham Prajapati <exploshubhs@gmail.com>
2018-04-01 21:10:11 +0530

    Created New Table Saleesperson 01042018

---
+++ NewTable/Salesperson.sql
@@ -0,0 +1,4 @@
+"ERROR:  syntax error at or near "CREATE"
+LINE 1: ...ARE "CURSOR:CONN:5567509" CURSOR WITHOUT HOLD FOR CREATE TAB...
+                                                             ^
+"
